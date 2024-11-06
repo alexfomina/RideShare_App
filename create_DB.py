87deb86d6cbef779d2455492cfa8b2321b5c3cf1
Henry Sauermann <115606915+henrysau@users.noreply.github.com>
2024-11-05 19:19:57 -0800

    Create create_DB.py

---
+++ create_DB.py
@@ -0,0 +1,19 @@
+import mysql.connector
+conn = mysql.connector.connect(host = 'localhost',
+                               user = 'root',
+                               password = 'HenryCPSC408',
+                               auth_plugin = 'mysql_native_password')
+
+cur_obj = conn.cursor()
+
+cur_obj.execute("CREATE DATABASE RideShare;")
+
+cur_obj.execute("SHOW DATABASES;")
+
+for row in cur_obj:
+    print(row)
+
+
+
+print(cur_obj)
+conn.close()
