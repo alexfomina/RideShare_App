b4e717068a55f00cd303e2fe5ed14024ee2ff6cf
Alexandra Fomina <fomina@chapman.edu>
2024-11-05 16:43:58 -0800

    Updating app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from db import db_operations
+from db import db
 
 def main_menu():
     print("Welcome to RideSHare!")
@@ -16,3 +16,28 @@ def create_account():
         db.create_driver_account()
     else:
         create_account()
+
+def driver_menu():
+#View Rating: This will show the driver their current rating. This will be the
+#average rating of all rides they have given.
+#b.
+#View Rides: This
+#will show the driver the list of all rides they have given.
+#c.
+#Activate/Deactivate Driver Mode: This updates a flag on their profile, letting
+#riders know if they are accepting new rides right no
+    user_choice = input('''
+    WELCOME DRIVER!
+    SELECT FROM THE FOLLOWING MENU:
+    1. View your rating
+    2. View Rides
+    3. Activate/Deactivate Driver Mode
+    4. Exit
+    ''')
+    if user_choice == 1:
+        printf("Current rating is ...")
+        db.get_rating()
+    elif user_choice == 4:
+        printf("Thank you!")
+    else:
+        return 0
